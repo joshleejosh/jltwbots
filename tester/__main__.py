d00c8dcd92de28f85331b92ff0a6edaf2c669284
Josh Lee <josh@floor.is>
2016-11-25 17:14:01 -0800

    tester script is a search script

--- tester/__main__.py
+++ tester/__main__.py
@@ -3,16 +3,19 @@
 import argparse
 import jltw
 
-def test_me(fn):
+def test_me(fn, q):
     api = jltw.open_twitter(fn)
-    tweets = api.GetUserTimeline()
-    print api.VerifyCredentials().screen_name
+    #tweets = api.GetUserTimeline()
+    #print api.VerifyCredentials().screen_name
+    tweets = api.GetSearch('q='+q, count=200)
     for tweet in tweets:
-        print tweet.text
+        s = '%s: %s'%(tweet.user.screen_name, tweet.text)
+        print s.encode('utf-8')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('authfile', type=str, help='File containing Twitter credentials')
+    parser.add_argument('searchstring', type=str, help='A string to search for')
     args = parser.parse_args()
-    test_me(args.authfile)
+    test_me(args.authfile, args.searchstring)
 
