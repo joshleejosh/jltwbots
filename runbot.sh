d9f2646710e32c59157ac6e7431b61757a5f5de1
Josh Lee <josh@floor.is>
2016-11-27 09:28:01 -0800

    Make sure working dir is in python path

--- runbot.sh
+++ runbot.sh
@@ -5,5 +5,7 @@
 # Make sure we're in the correct directory in case there are any relative paths involved.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${DIR}
-botenv/bin/python $*
+source botenv/bin/activate
+export PYTHONPATH=${PYTHONPATH}:${DIR}
+python $*
 
