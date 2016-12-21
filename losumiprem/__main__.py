d6d54de770f9cbdff34f5a109f78f003e8974981
Josh Lee <josh@floor.is>
2016-12-20 19:03:09 -0800

    losumiprem generates a grammarizer grammar

--- losumiprem/__main__.py
+++ losumiprem/__main__.py
@@ -1,21 +1,17 @@
-import argparse, random
-import jltw.mru
+import os, json, codecs, argparse, random
 from losumiprem.rhymer import set_verbosity, find_rhymes
 from losumiprem.filters import in_names, is_wordnet_proper
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('grammarfile',
+            help='output grammar file')
     parser.add_argument('-n', '--num-out',
             dest='numout',
             action='store',
             type=int,
             default=1,
             help='number of rhymes to generate')
-    parser.add_argument('-m', '--mru-file',
-            dest='mrufile',
-            action='store',
-            default='',
-            help='mru list for tracking duplicates')
     parser.add_argument('-v', '--verbose',
             dest='verbose',
             action='count',
@@ -29,15 +25,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     set_verbosity(args.verbose - args.quiet)
-    mru = jltw.mru.MRU(args.mrufile)
-    mru.load()
 
     words = [
-        'L AO1 R AH0 M',
-        'IH1 P S AH0 M',
-        'D OW1 L AO0 R',
-        'S IH1 T',
-        'AH0 M EH1 T',
+        u'L AO1 R AH0 M',
+        u'IH1 P S AH0 M',
+        u'D OW1 L AO0 R',
+        u'S IH1 T',
+        u'AH0 M EH1 T',
     ]
 
     rhymes = []
@@ -46,14 +40,15 @@ if __name__ == '__main__':
         a = [w.title() if in_names(w) or is_wordnet_proper(w) else w for w in a]
         rhymes.append(a)
 
-    for i in xrange(args.numout):
-        s = ''
-        while not s or s in mru:
-            s = ' '.join((random.choice(w) for w in rhymes))
-            s = s[0].upper() + s[1:]
-            s += '.'
-        print s
-        mru.add(s)
-
-    mru.save()
+    grammar = {
+        '*': '{Lorem} {ipsum} {dolor} {sit} {amet}.',
+        'lorem': rhymes[0],
+        'ipsum': rhymes[1],
+        'dolor': rhymes[2],
+        'sit': rhymes[3],
+        'amet': rhymes[4],
+    }
+    args.grammarfile = os.path.realpath(args.grammarfile)
+    with codecs.open(args.grammarfile, 'w', encoding='utf-8') as fp:
+        json.dump(grammar, fp, indent=1)
 
